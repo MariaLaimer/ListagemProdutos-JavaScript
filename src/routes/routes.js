4b02d721b55000b5ea2243ef2047a0213293d00b
Leo981-web <leozancanaro@outlook.com.br>
2026-04-17 17:09:22 -0300

    Correção da rolagem do App

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,16 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
-import { createStackNavigator } from "@react-navigation/stack";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import ProductList from "../screens/ProductList";
 import ProductDetail from "../screens/ProductDetail";
 import Login from "../screens/Login";
 
-const AppStack = createStackNavigator();
+const AppStack = createNativeStackNavigator();
 
 export default function Routes() {
   return (
     <NavigationContainer>
       <AppStack.Navigator>
-        <AppStack.Screen name="Login" component={Login} />
+        <AppStack.Screen name="Login" component={Login} options={{ headerShown: false }} />
         <AppStack.Screen name="ProductList" component={ProductList} />
         <AppStack.Screen name="ProductDetail" component={ProductDetail} />
       </AppStack.Navigator>
